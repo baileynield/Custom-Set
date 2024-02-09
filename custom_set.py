908453da6a713e4ddcc9271e421a09d1d94a0b5b
Bailey Nield <bailey.nield18@gmail.com>
2024-02-09 10:00:18 -0700

    Create remove method

--- custom_set.py
+++ custom_set.py
@@ -9,7 +9,10 @@ class CustomSet():
             self.items.append(single_item)
         
     def remove(self, single_item: str):
-        pass
+        self.single_item = single_item
+        if single_item in self.items:
+            self.items.remove(single_item
+)
     def as_list(self) -> list:
         return self.items
     def clear(self):
@@ -20,6 +23,8 @@ def main():
     my_set = CustomSet()
     my_set.add("item 1")
     my_set.add("item 2")
+    my_set.add("item 1")
+    my_set.remove("item 2")
     print(my_set.as_list())
 
 
