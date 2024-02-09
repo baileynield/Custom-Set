8097547e2e96b1b7f29a0dd9ce043c42b568c40d
Bailey Nield <bailey.nield18@gmail.com>
2024-02-09 08:59:16 -0700

    Create file with empty methods in CustomSet class

---
+++ custom_set.py
@@ -0,0 +1,17 @@
+class CustomSet():
+    def __init__(self):pass
+
+    def add((self, single_item: str):
+        pass
+    def remove(self, single_item: str):
+        pass
+    def as_list(self) -> list:
+        pass
+    def clear(self):
+        pass
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
